3d9cd66983e27f627281201eea63308d2174bf48
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-08-17 13:43:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,54 +40,58 @@ client.on('message', message => {
   var msglow = message.content.toLowerCase();
   if (msglow.startsWith(prefix)) {
     var msgnow = msglow.slice(4)
-    if (msgnow == "invite") {
-	    const InviteEmbed = new Discord.MessageEmbed()
-		.setColor('#ffff00')
-		.setTitle("Click this link")
-		.setURL("https://discord.com/api/oauth2/authorize?client_id=743150383496429649&scope=bot&permissions=281664")
-		.setTimestamp()
-		.setFooter('Aurelia Kingdom Bot', ProfilePicture);
-        	message.channel.send(InviteEmbed)
-    } else if (msgnow.includes("place")) {
-	    if (msgnow.includes("list")) {
-		msglow = "";
-		var array = Object.keys(place);
-		for (var key in array) {
-			msglow = msglow.concat("- "+array[key]+"\n")
-		}
-		message.channel.send("List tempat :\n```"+msglow+"```");
-	    } else {
-	    	message.channel.send("the place name ?")
-	    	const collectMessagePlace = new Discord.MessageCollector(message.channel,response => response.author.id == message.author.id, {time:10000});
-	    	collectMessagePlace.once('collect', response => {
-			msglow = response.content.toLowerCase();
-			if (place[msglow] == undefined) {
-				if (msglow == "auk-place") {
-				} else {
-					message.channel.send('Error : Place not found\nTry to use "auk-place list" instead')
-					collectMessagePlace.stop();	
+    switch (msgnow) {
+	    case "invite":
+		    const InviteEmbed = new Discord.MessageEmbed()
+			.setColor('#ffff00')
+			.setTitle("Click this link")
+			.setURL("https://discord.com/api/oauth2/authorize?client_id=743150383496429649&scope=bot&permissions=281664")
+			.setTimestamp()
+			.setFooter('Aurelia Kingdom Bot', ProfilePicture);
+		    message.channel.send(InviteEmbed)  
+		    break;
+	    case "place":
+		    message.channel.send("the place name ?")
+		    const collectMessagePlace = new Discord.MessageCollector(message.channel,response => response.author.id == message.author.id, {time:10000});
+		    collectMessagePlace.once('collect', response => {
+			    msglow = response.content.toLowerCase();
+			    if (place[msglow] == undefined) {
+				    if (msglow == "auk-place") {
+				    } else {
+					    message.channel.send('Error : Place not found\nTry to use "auk-place list" instead')
+					    collectMessagePlace.stop();	
 				}
-			} else {
-				sendEmbedPlace(place[msglow].name,place[msglow].trellolink,place[msglow].trellopic,message);
-			}
-		})
-	    }
-    } else if (msgnow == "?" || msgnow == "help") {
-	    msglow = "";
-	    for (const [key,value] of Object.entries(CommandList)) {
-		    msglow = msglow.concat(`${key} --> ${value}\n`)
-	    }
-	    const HelpEmbed = new Discord.MessageEmbed()
-		.setColor('#ffff00')
-		.setTitle('Aurelia Kingdom Bot')
-        	.addFields(
-	        { name: 'Command List', value: msglow, inline: false },
-		)
-		.setTimestamp()
-		.setFooter('Aurelia Kingdom Bot', ProfilePicture);
-	       message.channel.send(HelpEmbed)
-    } else {
-		message.channel.send("oops, use auk-?")
+			    } else {
+				    sendEmbedPlace(place[msglow].name,place[msglow].trellolink,place[msglow].trellopic,message);
+			    }
+		    })
+		    break;
+	    case "place list":
+		    msglow = "";
+		    var array = Object.keys(place);
+		    for (var key in array) {
+			    msglow = msglow.concat("- "+array[key]+"\n")
+		    }
+		    message.channel.send("List tempat :\n```"+msglow+"```");
+		    break;
+	    case "help":
+	    case "?":
+		    msglow = "";
+		    for (const [key,value] of Object.entries(CommandList)) {
+			    msglow = msglow.concat(`${key} --> ${value}\n`)
+		    }
+		    const HelpEmbed = new Discord.MessageEmbed()
+		    .setColor('#ffff00')
+		    .setTitle('Aurelia Kingdom Bot')
+		    .addFields(
+			    { name: 'Command List', value: msglow, inline: false },
+		    )
+		    .setTimestamp()
+		    .setFooter('Aurelia Kingdom Bot', ProfilePicture);
+		    message.channel.send(HelpEmbed)
+		    break;
+	    default:
+		    message.channel.send("coba gunakan auk-?");
     }
   }
 })
